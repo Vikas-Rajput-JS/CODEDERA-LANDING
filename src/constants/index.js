7c847fa31db235911c593ac37348fe7c56c58c20
Amit <amitk@jcsoftwaresolution.com>
2023-10-05 13:19:06 +0530

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -214,23 +214,23 @@ export const socialMedia = [
   {
     id: "social-media-1",
     icon: instagram,
-    link: "https://www.instagram.com/",
+    link: "https://www.instagram.com/x4_vikas.env",
   },
   {
     id: "social-media-2",
     icon: facebook,
     link: "https://www.facebook.com/",
   },
-  {
-    id: "social-media-3",
-    icon: twitter,
-    link: "https://www.twitter.com/",
-  },
-  {
-    id: "social-media-4",
-    icon: linkedin,
-    link: "https://www.linkedin.com/",
-  },
+  // {
+  //   id: "social-media-3",
+  //   icon: twitter,
+  //   link: "https://www.twitter.com/",
+  // },
+  // {
+  //   id: "social-media-4",
+  //   icon: linkedin,
+  //   link: "https://www.linkedin.com/",
+  // },
 ];
 
 export const clients = [
